9c90fce2d03470ad7c8afb69000b93cbd4aabef7
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2010-08-13 20:05:25 +0200

    optimized the FTI view

--- wiki/fulltext/all/index.js
+++ wiki/fulltext/all/index.js
@@ -1,21 +1,8 @@
 function(doc) {
-    var ret = new Document();
+  var ret = new Document();
 
-    function idx(obj) {
-    for (var key in obj) {
-        switch (typeof obj[key]) {
-        case 'object':
-        idx(obj[key]);
-        break;
-        case 'function':
-        break;
-        default:
-        ret.add(obj[key]);
-        break;
-        }
-    }
-    };
+  if (doc.title) ret.add(doc.title, {"field":"title"})
+  if (doc.text) ret.add(doc.title, {"field":"text"})
 
-    idx(doc);
-    return ret;
+  return ret;
 }
